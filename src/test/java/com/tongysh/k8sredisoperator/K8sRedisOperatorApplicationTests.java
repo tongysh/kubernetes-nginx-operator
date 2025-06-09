5dfe39bd72a4fbfb6d8a9f805b47dd566f3acdc0
tongysh <tongysh666@gmail.com>
2025-06-09 16:52:13 +0800

    init初始化项目

---
+++ src/test/java/com/tongysh/k8sredisoperator/K8sRedisOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tongysh.k8sredisoperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sRedisOperatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
